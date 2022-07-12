539a6718fcb4b5e5f9af37fecf71d15485328b29
David Tavarez <337107+davidtavarez@users.noreply.github.com>
2022-07-12 13:33:40 +0200

    fixing scripts for testnet

--- scripts/bash/test/close_wallet.sh
+++ scripts/bash/test/close_wallet.sh
@@ -3,7 +3,6 @@
 iv=$(openssl rand -hex 12)
 payload=$(echo "{\"id\":\"$iv\",\"method\":\"close_wallet\",\"params\":{\"name\":null}}")
 payload=$(.venv/bin/python ./scripts/python/encrypt.py "$1" "$iv" "$payload")
-unset password
 read body_enc nonce < <(echo $(curl -s --user grin:$(cat ~/.grin/test/.owner_api_secret) -d '{"jsonrpc":"2.0", "id":"'"$iv"'","method":"encrypted_request_v3","params":{"nonce":"'"$iv"'","body_enc":"'"$payload"'"}}' -o - http://127.0.0.1:3420/v3/owner | jq -r '.result.Ok.body_enc, .result.Ok.nonce'))
 unset payload
 result=$(.venv/bin/python ./scripts/python/decrypt.py $1 $nonce $body_enc)
