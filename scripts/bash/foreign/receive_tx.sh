f3ffef916aa79e7e008e17ee854570cb168f68f6
David Tavarez <337107+davidtavarez@users.noreply.github.com>
2022-07-26 08:00:42 +0200

    removing line

--- scripts/bash/foreign/receive_tx.sh
+++ scripts/bash/foreign/receive_tx.sh
@@ -2,8 +2,7 @@
 
 iv=$(openssl rand -hex 12)
 slate=$(cat $2)
-slate=$(echo "${slate//[$'\t\r\n']}")
-slate=$(echo "$slate" | sed 's/ //g')
+slate=$(echo "${slate//[$'\t\r\n']}" | sed 's/ //g')
 result=`ALL_PROXY=socks5h://localhost:9050 http POST $(php scripts/php/grin1.php -a $1) jsonrpc="2.0" id=$iv method="receive_tx" params[]:=$slate params[]=null params[]=null`
 if $(echo $result | jq 'has("error")')
 then
