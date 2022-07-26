f8e875235f5b79ecfdacd01332031285ec5c5615
David Tavarez <337107+davidtavarez@users.noreply.github.com>
2022-07-26 07:57:59 +0200

    escaping slate

--- scripts/bash/foreign/receive_tx.sh
+++ scripts/bash/foreign/receive_tx.sh
@@ -2,6 +2,8 @@
 
 iv=$(openssl rand -hex 12)
 slate=$(cat $2)
+slate=$(echo "${slate//[$'\t\r\n']}")
+slate=$(echo "$slate" | sed 's/ //g')
 result=`ALL_PROXY=socks5h://localhost:9050 http POST $(php scripts/php/grin1.php -a $1) jsonrpc="2.0" id=$iv method="receive_tx" params[]:=$slate params[]=null params[]=null`
 if $(echo $result | jq 'has("error")')
 then
